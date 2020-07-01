c3dbaaca408f0f842c6ddd24906e507f627de618
Rodrigo Fernandes <45701541+rfernand3s@users.noreply.github.com>
2020-07-01 12:32:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import pydeck as pdk
 
 
 dataframe = pd.read_csv('criminalidade_sp_2.csv')
@@ -14,8 +15,39 @@ st.markdown("""
     Índices de criminalidade.
 """)
 
+st.sidebar.info("Foram carregadas {} linhas.".format(dataframe.shape[0]))
+
 if st.sidebar.checkbox("Mostrar tabela?"):
     st.header("Raw Data")
     st.write(dataframe)
 
-st.sidebar.info("Foram carregadas {} linhas.".format(dataframe.shape[0]))
+df.time = pd.to_datetime(df.time)
+ano_selecionado = st.sidebar.slider("Selecione um ano", 2010,2018,2015)
+df_selected = df[df.time.dt.year == ano_selecionado]
+
+## st.map(df_selected)
+## mapa
+st.subheader("Mapa da Criminalidade")
+## st.map(df)
+st.pydeck_chart(pdf.Deck(
+    initial_view_state = pdf.ViewState(
+        latitude=-23.567145,
+        longitude=-46.648936,
+        zoom=8,
+        pitch=50
+    ),
+    layers=[
+        pdk.Layer(
+            'HexagonLayer',
+            data=df_selected[['latitude', 'longitude']],
+            get_position='[longitude,latitude]',
+            auto_highlight=True,
+            elevation_scale=50,
+            pickable=True,
+            elevation_range=[0, 3000],
+            extruded=True,
+            coverage=1
+        )
+    ],
+))
+
